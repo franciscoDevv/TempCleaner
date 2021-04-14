3a084b47f6ea72e2f8539d56ab82b351d9603229
Francisco <77551844+FrancisCode13@users.noreply.github.com>
2021-04-14 10:49:58 -0400

    Update TempCleaner.bat

--- TempCleaner.bat
+++ TempCleaner.bat
@@ -1,6 +1,44 @@
-@echo off
+title Cleaner
+color e
+mode 120,50
+:inicio
+echo.
+echo.
+echo ================================================
+echo =                   MENU                      
+echo ================================================
+echo =
+echo =          1. Clear Temp
+echo =          2. Clear Prefetch
+echo =          3. Exit
+echo =
+echo ================================================
+echo.
+echo.
+set /p menu=Opcion = 
+
+if "%menu%"=="1" goto op1
+if "%menu%"=="2" goto op2
+if "%menu%"=="3" goto salir
+
+:op1
+cls
 cd "C:\Windows\Temp"
-del *
-echo Gracias por usar TempCleaner
-pause
-exit
+del * /Q /F
+pause>nul
+cls
+goto inicio
+
+:op2
+cls
+cd "C:\Windows\Prefetch"
+del * /Q /F
+pause>nul
+cls
+goto inicio
+
+:salir
+cls
+echo Gracias por usar Temporary Cleanser
+pause>nul
+cls&exit
